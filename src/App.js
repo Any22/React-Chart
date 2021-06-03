67c888c30092aed3a9f25dc1be0d960fe8e689cd
Saba Akhtar <sabasaeedakhtar@gmail.com>
2021-06-03 22:58:36 +1000

    created BarChart component

--- src/App.js
+++ src/App.js
@@ -1,46 +1,16 @@
 import React from 'react';
-import {Bar, Doughnut,Line,Pie} from 'react-chartjs-2';
+import BarChart from './BarChart';
 // git remote add origin https://github.com/Any22/React-Chart.git
 // git branch -M main
 // git push -u origin main
 
-const state = {
-  labels: ['January', 'February', 'March',
-           'April', 'May'],
-  datasets: [
-    {
-      label: 'Rainfall',
-      fill:false,
-      lineTension:0.5,
-      backgroundColor: 'rgba(75,192,192,1)',
-      borderColor: 'rgba(0,0,0,1)',
-      borderWidth: 2,
-      data: [65, 59, 80, 81, 56]
-    }
-  ]
-}
-
-export default  function App() {
-  
-    return (
+function App() {
+  return (
       <>
-      <div>
+      <div className="bar-chart">
         <h3>Bar Chart</h3>
-        <Bar
-          data={state}
-          options={{
-            title:{
-              display:true,
-              text:'Average Rainfall per month',
-              fontSize:20
-            },
-            legend:{
-              display:true,
-              position:'right'
-            }
-          }}
-          colors={['#a8e0ff', '#8ee3f5', '#70cad1', '#3e517a', '#b08ea2', '#BBB6DF']}
-        />
+         <BarChart/>
+{/* 
       </div>
       <div>
         <h3>Line Chart</h3>
@@ -95,8 +65,9 @@ export default  function App() {
           },
         }}
         colors={['#a8e0ff', '#8ee3f5', '#70cad1', '#3e517a', '#b08ea2', '#BBB6DF']}
-        />
+        /> */} 
       </div>
       </>
     );
   }
+export default App;
